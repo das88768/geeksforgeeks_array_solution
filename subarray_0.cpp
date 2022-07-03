d884eea5c375f419bd65ccb439d0ed6f6514e0d3
Akash Das <das88768@gmail.com>
2022-07-03 10:05:35 +0530

    subarray of zero of sum

---
+++ subarray_0.cpp
@@ -0,0 +1,59 @@
+// Problem Name: Subarray with 0 sum.
+
+// { Driver Code Starts
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        //Your code here
+        unordered_set<int> m;
+        int i, prefix=0;
+        for(i=0;i<n;i++){
+            prefix = prefix + arr[i];
+            if(prefix == 0){
+                return true;
+            }
+            else{
+                if(m.find(prefix) != m.end()){
+                    return true;
+                }
+                else{
+                    m.insert(prefix);
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}  // } Driver Code Ends
